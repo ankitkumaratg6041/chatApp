46409defc2aebbd8565c25a632be2781028588f8
ankitkumaratg6041 <ankitkumaratg6041@gmail.com>
2025-06-22 22:13:58 -0600

    chat application created

---
+++ server/src/socket.ts
@@ -0,0 +1,32 @@
+import { Server, Socket } from "socket.io";
+import prisma from "./config/db.config.js";
+
+interface CustomSocket extends Socket {
+  room?: string;
+}
+export function setupSocket(io: Server) {
+  io.use((socket: CustomSocket, next) => {
+    const room = socket.handshake.auth.room || socket.handshake.headers.room;
+    if (!room) {
+      return next(new Error("Invalid room"));
+    }
+    socket.room = room;
+    next();
+  });
+
+  io.on("connection", (socket: CustomSocket) => {
+    // * Join the room
+    socket.join(socket.room);
+
+    socket.on("message", async (data) => {
+        await prisma.chats.create({
+          data: data
+        })
+        socket.to(socket.room).emit("message", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("A user disconnected:", socket.id);
+    });
+  });
+}
